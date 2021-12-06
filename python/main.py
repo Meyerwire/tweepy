63bda866b194c9c82b0f6396f860381b757543f8
Meyer <jeremymakoben@yahoo.de>
2021-12-06 16:59:08 +0100

    Added some detail
    Stop after 5 failed auth. function for tweeting

--- python/main.py
+++ python/main.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 import tweepy
+
+#Functions
+def helloTweep():
+    api.update_status("Hello Tweepy")
+    print("tweeted")
+
+#Variables
 auth_check = False
+Fail_counter = 0
+
+
+#Auth Loop
 while auth_check == False:
     key = input('key: ')
     key_private = input('key secret: ')
     token = input('token: ')
     token_secret = input('token secret: ')
+
 
 
     # Authenticate to Twitter
@@ -20,7 +32,13 @@ while auth_check == False:
         auth_check = True
     except:
         print("Error during authentication")
+        Fail_counter += 1
+        if Fail_counter > 4:
+            break
+        
+
+if auth_check == True:
+    helloTweep()
 
 
-api.update_status("Hello Tweepy")
-print("tweeted")
+
